1ce7b2da7680872604b8f6cd50657c9fc55dbfb5
Moterakeg <chachazack09@gmail.com>
2026-04-12 15:37:38 +0300

    fix stk push frontend

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
-console.log("🚀 Test file started");
-
-const axios = require("axios");
-
-(async () => {
-  try {
-    console.log("📡 Sending request...");
-
-    const res = await axios.post("http://127.0.0.1:3000/stkpush", {
-      phone: "254746038656",
-      amount: 4500,
-      reference: "TEST123"
-    });
-
-    console.log("✅ RESPONSE RECEIVED:");
-    console.log(res.data);
-
-  } catch (err) {
-    console.log("❌ ERROR:");
-    console.log(err.response?.data || err.message);
-  }
-})();
+fetch("https://mpesa-backend-rakq.onrender.com/stkpush", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    phone: "254746038656",
+    amount: 1
+  })
+})
